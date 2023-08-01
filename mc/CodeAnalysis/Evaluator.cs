c147e8b6b4f6180870201b5fa46a91d19ded0e16
Reecion <61022054+Reecion@users.noreply.github.com>
2023-08-01 19:58:40 +0000

    Replace switch with operator lookup

--- mc/CodeAnalysis/Evaluator.cs
+++ mc/CodeAnalysis/Evaluator.cs
@@ -28,7 +28,7 @@ namespace Minsk.CodeAnalysis
             {
                 var operand = EvaluateExpression(u.Operand);
 
-                switch (u.OperatorKind)
+                switch (u.Op.Kind)
                 {
                     case BoundUnaryOperatorKind.Identity:
                         return (int) operand;
@@ -37,7 +37,7 @@ namespace Minsk.CodeAnalysis
                     case BoundUnaryOperatorKind.LogicalNegation:
                         return !(bool) operand;
                     default:
-                        throw new Exception($"Unexpected unary operator {u.OperatorKind}");
+                        throw new Exception($"Unexpected unary operator {u.Op}");
                 }
 
             }
@@ -47,7 +47,7 @@ namespace Minsk.CodeAnalysis
                 var left = EvaluateExpression(b.Left);
                 var right = EvaluateExpression(b.Right);
 
-                switch (b.OperatorKind)
+                switch (b.Op.Kind)
                 {
                     case BoundBinaryOperatorKind.Addition:
                         return (int) left + (int) right;
@@ -62,7 +62,7 @@ namespace Minsk.CodeAnalysis
                     case BoundBinaryOperatorKind.LogicalOr:
                         return (bool) left || (bool) right;
                     default:
-                        throw new Exception($"Unexpected binary operator {b.OperatorKind}");
+                        throw new Exception($"Unexpected binary operator {b.Op}");
                 }
 
             }

40441039d17172aba6ceda49452c6aedcb4c2420
Reecion <61022054+Reecion@users.noreply.github.com>
2023-08-03 15:41:33 +0000

    SyntaxTokens test

--- Minsk.Tests/CodeAnalysis/Syntax/UnitTest1.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/UnitTest1.cs
@@ -1,32 +1,59 @@
 using System;
-using System.Collection.Generic;
-using Minsk.CodeAnalysis.Syntax;
+using System.Collections.Generic;
 
 namespace Minsk.Tests.CodeAnalysis.Syntax;
 
 public class LexerTest
 {
     [Theory]
+    [MemberData(nameof(GetTokensData))]
     public void Lexer_Lexes_Token(SyntaxKind kind, string text)
     {
         var tokens = SyntaxTree.ParseTokens(text);
 
         var token = Assert.Single(tokens);
-        Assert.Equals(kind, token.Kind);
-        Assert.Equals(text, token.Text);
+        Assert.Equal(kind, token.Kind);
+        Assert.Equal(text, token.Text);
     }
 
     public static IEnumerable<object[]> GetTokensData()
     {
-        foreach (vat t in GetTokens())
-            yield return new object[] {t.Kind, t.text };
+        foreach (var t in GetTokens())
+            yield return new object[] {t.kind, t.text };
     }
 
-    private static IEnumerable<(SyntaxKind kind1, string text)> GetTokens()
+    private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
     {
-        return new[] {
+        return new[] 
+        {
+            (SyntaxKind.PlusToken, "+"),
+            (SyntaxKind.MinusToken, "-"),
+            (SyntaxKind.StarToken, "*"),
+            (SyntaxKind.SlashToken, "/"),
+            (SyntaxKind.BangToken, "!"),
+            (SyntaxKind.AmpersandAmpersandToken, "&&"),
+            (SyntaxKind.PipePipeToken, "||"),
+            (SyntaxKind.EqualsToken, "="),
+            (SyntaxKind.EqualsEqualsToken, "=="),
+            (SyntaxKind.BangEqualsToken, "!="),
+
+            (SyntaxKind.OpenParethesisToken, "("),
+            (SyntaxKind.CloseParenthesisToken, ")"),
+
+            (SyntaxKind.TrueKeyword, "true"),
+            (SyntaxKind.FalseKeyword, "false"),
+
+            (SyntaxKind.NumberToken, "1"),
+            (SyntaxKind.NumberToken, "123"),
+
             (SyntaxKind.IdentifierToken, "a"),
-            (SyntaxKind.IdentifierToken, "abc")
-        }
+            (SyntaxKind.IdentifierToken, "abc"),
+
+            (SyntaxKind.WhiteSpaceToken, " "),
+            (SyntaxKind.WhiteSpaceToken, "  "),
+            (SyntaxKind.WhiteSpaceToken, "\r"),
+            (SyntaxKind.WhiteSpaceToken, "\n"),
+            (SyntaxKind.WhiteSpaceToken, "\r\n")
+        };
     }
 }

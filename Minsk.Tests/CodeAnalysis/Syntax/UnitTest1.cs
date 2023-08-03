283851c26ea2497efb167162a2f42fe83d89e078
Reecion <61022054+Reecion@users.noreply.github.com>
2023-08-03 15:20:44 +0000

    First test

--- Minsk.Tests/CodeAnalysis/Syntax/UnitTest1.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/UnitTest1.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collection.Generic;
 using Minsk.CodeAnalysis.Syntax;
-using Xunit;
 
 namespace Minsk.Tests.CodeAnalysis.Syntax;
 
@@ -9,13 +9,24 @@ public class LexerTest
     [Theory]
     public void Lexer_Lexes_Token(SyntaxKind kind, string text)
     {
-        var tokens = SyntaxTree.Parse(text);
+        var tokens = SyntaxTree.ParseTokens(text);
+
+        var token = Assert.Single(tokens);
+        Assert.Equals(kind, token.Kind);
+        Assert.Equals(text, token.Text);
+    }
+
+    public static IEnumerable<object[]> GetTokensData()
+    {
+        foreach (vat t in GetTokens())
+            yield return new object[] {t.Kind, t.text };
     }
 
     private static IEnumerable<(SyntaxKind kind1, string text)> GetTokens()
     {
         return new[] {
-            (SyntaxKind.)
+            (SyntaxKind.IdentifierToken, "a"),
+            (SyntaxKind.IdentifierToken, "abc")
         }
     }
 }
